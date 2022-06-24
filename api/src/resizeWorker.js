9ca0f0d3166f0ec418c9ace38d7f7dec777f1f75
qazi112 <qarsalan13@gmail.com>
2022-06-24 12:02:25 +0500

    bug fix 1 submission mod4 - 4th sol

--- api/src/resizeWorker.js
+++ api/src/resizeWorker.js
@@ -5,6 +5,6 @@ gm(workerData.source)
     .resize(100, 100)
     .write(workerData.destination, (error)=>{
         if (error) throw error
-        parentPort.postMessage(resized, true)
-        
+        parentPort.postMessage({resized: true})
+
     })
