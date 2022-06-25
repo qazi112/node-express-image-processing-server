f4b1f5761885c2cf911adaecd5c65f0fc9e4589e
qazi112 <qarsalan13@gmail.com>
2022-06-26 01:16:30 +0500

    bug fix 1 submission mod4 - 4th sol

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -1,8 +1,8 @@
 const path = require("path")
 const { Worker, isMainThread } = require("worker_threads")
 
-const pathToResizeWorker = path.resolve(__dirname, "./resizeWorker.js")
-const pathToMonochromeWorker = path.resolve(__dirname, "./monochromeWorker.js")
+const pathToResizeWorker = path.resolve(__dirname, "resizeWorker.js")
+const pathToMonochromeWorker = path.resolve(__dirname, "monochromeWorker.js")
 
 console.log(pathToResizeWorker)
 const uploadPathResolver = (filename) => {
